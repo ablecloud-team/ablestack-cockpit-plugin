62c42c621fb8f6cd716ec150e5e1974a1d0e1440
ktcheon <ktc7270@gmail.com>
2021-03-09 16:57:47 +0900

    commit on 'PCS'

--- python/PCS/pacemaker.py
+++ python/PCS/pacemaker.py
@@ -38,51 +38,46 @@ class Pacemaker:
         self.cluster_name = cluster_name
         self.hostnames = hostnames
 
-        call(['echo', 'pcs', 'host', 'auth', '-u', 'hacluster', '-p', 'password', *hostnames])
-        call(['echo', 'pcs', 'cluster', 'setup', self.cluster_name, *hostnames])
-        call(['echo', 'pcs', 'cluster', 'start', '--all'])
-        call(['echo', 'systemctl', 'enable', '--now', 'corosync.service'])
-        call(['echo', 'systemctl', 'enable', '--now', 'pacemaker.service'])
-        call(['echo', 'pcs', 'property', 'set', 'stonith-enabled=false'])
+        call(['pcs', 'host', 'auth', '-u', 'hacluster', '-p', 'password', *hostnames])
+        call(['pcs', 'cluster', 'setup', self.cluster_name, *hostnames])
+        call(['pcs', 'cluster', 'start', '--all'])
+        call(['systemctl', 'enable', '--now', 'corosync.service'])
+        call(['systemctl', 'enable', '--now', 'pacemaker.service'])
+        call(['pcs', 'property', 'set', 'stonith-enabled=false'])
 
 
     def createResource(self, resource_name, xml_path):
         self.resource_name = resource_name
         self.xml_path = xml_path
 
-        call(['echo', 'pcs', 'resource', 'create', self.resource_name, 'VirtualDomain', 'hypervisor="qemu:///system"', 
+        call(['pcs', 'resource', 'create', self.resource_name, 'VirtualDomain', 'hypervisor="qemu:///system"', 
         'config=', self.xml_path, 'migration_transport=ssh', 'op', 'start', 'timeout="120s"', 'op', 'monitor', 'timeout="30"', 
         'nterval="10"', 'meta', 'allow-migrate="true"', 'priority="100"'])
 
     def enableResource(self, resource_name):
         self.resource_name = resource_name
         
-        call(['echo', 'pcs', 'resource', 'cleanup', resource_name])
-        call(['echo', 'pcs', 'resource', 'enable', self.resource_name])
+        call(['pcs', 'resource', 'cleanup', resource_name])
+        call(['pcs', 'resource', 'enable', self.resource_name])
 
     def disableResource(self, resource_name):
         self.resource_name = resource_name
         
-        call(['echo', 'pcs', 'resource', 'disable', self.resource_name])
+        call(['pcs', 'resource', 'disable', self.resource_name])
 
     def moveResource(self, resource_name, target_host):
         self.resource_name = resource_name
         self.target_host = target_host
         
-        call(['echo', 'pcs', 'resource', 'move', self.resource_name, self.target_host])
+        call(['pcs', 'resource', 'move', self.resource_name, self.target_host])
 
     def cleanupResource(self, resource_name):
         self.resource_name = resource_name
         
-        call(['echo', 'pcs', 'resource', 'cleanup', self.resource_name])
+        call(['pcs', 'resource', 'cleanup', self.resource_name])
         
         
     def statusResource(self, resource_name):
         self.resource_name = resource_name
         
-        call(['echo', 'pcs', 'status', 'resources', '|grep', self.resource_name])
-
-
-a = Pacemaker()
-s = a.statusResource('dd')
-d = check_output(s)
+        call(['pcs', 'status', 'resources'])
