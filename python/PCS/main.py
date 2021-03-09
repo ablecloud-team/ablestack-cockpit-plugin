79f7452ac44731af46f4a021b2b56276340092c9
ktcheon <ktc7270@gmail.com>
2021-03-09 14:01:19 +0900

    commit on 'PCS'

---
+++ python/PCS/main.py
@@ -0,0 +1,43 @@
+from pacemaker import *
+
+def main():
+    pcs = Pacemaker()
+    args = parseArgs()
+
+    if (args.action) == 'config':
+        try:
+            pcs.configCluster(args.cluster, args.hosts)
+        except Exception as e:
+            print(e)
+    elif (args.action) == 'create':
+        try:
+            pcs.createResource(args.resource, args.xml)
+        except Exception as e:
+            print(e)
+    elif (args.action) == 'enable':
+        try:
+            pcs.enableResource(args.resource)
+        except Exception as e:
+            print(e)
+    elif (args.action) == 'disable':
+        try:
+            pcs.disableResource(args.resource)
+        except Exception as e:
+            print(e)
+    elif (args.action) == 'move':
+        try:
+            pcs.moveResource(args.resource, args.target)
+        except Exception as e:
+            print(e)
+    elif (args.action) == 'cleanup':
+        try:
+            pcs.cleanupResource(args.resource)
+        except Exception as e:
+            print(e)
+    else:
+        print('??????')
+
+
+
+if __name__ == "__main__":
+    main()
