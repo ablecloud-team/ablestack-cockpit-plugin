f2334a7390b40549779a27d139accbc92945e7f4
ktcheon <ktc7270@gmail.com>
2021-03-12 14:25:24 +0900

    commit on 'PCS'

--- python/PCS/main.py
+++ python/PCS/main.py
@@ -1,53 +1,30 @@
 #!/usr/bin/python3
 #-*- coding: utf-8 -*-
 
-from python.PCS.pacemaker import *
-from python.PCS.able_return import *
+from pacemaker import *
+from able_return import *
+import sys
 
 def main():
     pcs = Pacemaker()
     args = parseArgs()
 
     if (args.action) == 'config':
-        try:
-            pcs.configCluster(args.cluster, args.hosts)
-        except Exception as e:
-            print(e)
+        pcs.configCluster(args.cluster, args.hosts)
     elif (args.action) == 'create':
-        try:
-            pcs.createResource(args.resource, args.xml)
-        except Exception as e:
-            print(e)
+        pcs.createResource(args.resource, args.xml)
     elif (args.action) == 'enable':
-        try:
-            pcs.enableResource(args.resource)
-        except Exception as e:
-            print(e)
+        pcs.enableResource(args.resource)
     elif (args.action) == 'disable':
-        try:
-            pcs.disableResource(args.resource)
-        except Exception as e:
-            print(e)
+        pcs.disableResource(args.resource)
     elif (args.action) == 'move':
-        try:
-            pcs.moveResource(args.resource, args.target)
-        except Exception as e:
-            print(e)
+        pcs.moveResource(args.resource, args.target)
     elif (args.action) == 'cleanup':
-        try:
-            pcs.cleanupResource(args.resource)
-        except Exception as e:
-            print(e)
+        pcs.cleanupResource(args.resource)
     elif (args.action) == 'status':
-        try:
-            pcs.statusResource()
-        except Exception as e:
-            print(e)
-    else:
-        print('no match actions')
-        sys.exit(0)
-
+        pcs.statusResource()
 
 
 if __name__ == "__main__":
+    
     main()
