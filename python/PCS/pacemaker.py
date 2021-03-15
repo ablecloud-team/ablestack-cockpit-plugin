eba8c78dee7b97e4fb94b93cf60da2567afe900a
ktcheon <ktc7270@gmail.com>
2021-03-15 00:51:09 -0700

    commit on 'PCS'

--- python/PCS/pacemaker.py
+++ python/PCS/pacemaker.py
@@ -3,8 +3,10 @@
 
 import argparse
 from subprocess import check_output
-from subprocess import run
 from able_return import *
+from sh import pcs
+from sh import systemctl
+import sys
 
 def parseArgs():
     parser = argparse.ArgumentParser(description='Pacemaker cluster',
@@ -30,12 +32,14 @@ class Pacemaker:
         self.cluster_name = cluster_name
         self.hostnames = hostnames
 
-        run(['pcs', 'host', 'auth', '-u', 'hacluster', '-p', 'password', *hostnames])
-        run(['pcs', 'cluster', 'setup', self.cluster_name, *hostnames])
-        run(['pcs', 'cluster', 'start', '--all'])
-        run(['systemctl', 'enable', '--now', 'corosync.service'])
-        run(['systemctl', 'enable', '--now', 'pacemaker.service'])
-        run(['pcs', 'property', 'set', 'stonith-enabled=false'])
+
+
+        pcs('host', 'auth', '-u', 'hacluster', '-p', 'password', *hostnames)
+        pcs('cluster', 'setup', self.cluster_name, *hostnames)
+        pcs('cluster', 'start', '--all')
+        systemctl('enable', '--now', 'corosync.service')
+        systemctl('enable', '--now', 'pacemaker.service')
+        pcs('property', 'set', 'stonith-enabled=false')
 
         ret_val = {'cluster name :':self.cluster_name, 'hosts': hostnames}
         ret = createReturn(code=200, val=ret_val)
@@ -48,9 +52,9 @@ class Pacemaker:
         self.resource_name = resource_name
         self.xml_path = xml_path
 
-        run(['pcs', 'resource', 'create', self.resource_name, 'VirtualDomain', 'hypervisor="qemu:///system"', 
+        pcs('resource', 'create', self.resource_name, 'VirtualDomain', 'hypervisor="qemu:///system"', 
         'config=', self.xml_path, 'migration_transport=ssh', 'op', 'start', 'timeout="120s"', 'op', 'monitor', 'timeout="30"', 
-        'nterval="10"', 'meta', 'allow-migrate="true"', 'priority="100"'])
+        'interval="10"', 'meta', 'allow-migrate="true"', 'priority="100"')
         
         ret_val = {'resource name :':self.resource_name, 'hypervisor':'qemu:///system', 'config':self.xml_path, 'migration_transport':'ssh'}
         ret = createReturn(code=200, val=ret_val)
@@ -61,8 +65,8 @@ class Pacemaker:
     def enableResource(self, resource_name):
         self.resource_name = resource_name
         
-        run(['pcs', 'resource', 'cleanup', resource_name])
-        run(['pcs', 'resource', 'enable', self.resource_name])
+        pcs('resource', 'cleanup', resource_name)
+        pcs('resource', 'enable', self.resource_name)
         
         ret = createReturn(code=200, val='enable')
         print(json.dumps(json.loads(ret), indent=4))
@@ -72,7 +76,7 @@ class Pacemaker:
     def disableResource(self, resource_name):
         self.resource_name = resource_name
         
-        run(['pcs', 'resource', 'disable', self.resource_name])
+        pcs('resource', 'disable', self.resource_name)
         
         ret = createReturn(code=200, val='disable')
         print(json.dumps(json.loads(ret), indent=4))
@@ -82,22 +86,31 @@ class Pacemaker:
     def moveResource(self, resource_name, target_host):
         self.resource_name = resource_name
         self.target_host = target_host
-        res_output = check_output(['pcs', 'status', 'resources'], universal_newlines=True)
+        res_output = pcs('status', 'resources').stdout.decode().splitlines()
+        for line in res_output:
+            if self.resource_name in line:
+                current_host = line.split(' ')[-1]
         
-        current_host = res_output.split()[-1].strip('()') # current host
+        if current_host == self.target_host:
+            # print('현재 호스트로 마이그레이션 할 수 없습니다.')
+            ret = createReturn(code=500, val='FAIL')
+            print(json.dumps(json.loads(ret), indent=4))
+
+            sys.exit(1)
+
+        else:
+            pcs('resource', 'move', self.resource_name, self.target_host)
         
-        run(['pcs', 'resource', 'move', self.resource_name, self.target_host])
-        ret_val = {'current host :':current_host, 'target host':self.target_host}
-        
-        ret = createReturn(code=200, val=ret_val)
-        print(json.dumps(json.loads(ret), indent=4))
+            ret_val = {'current host':current_host, 'target host':self.target_host}
+            ret = createReturn(code=200, val=ret_val)
+            print(json.dumps(json.loads(ret), indent=4))
 
         return ret
 
     def cleanupResource(self, resource_name):
         self.resource_name = resource_name
         
-        run(['pcs', 'resource', 'cleanup', self.resource_name])
+        pcs('resource', 'cleanup', self.resource_name)
         
         ret = createReturn(code=200, val='cleanup')
         print(json.dumps(json.loads(ret), indent=4))
@@ -107,22 +120,25 @@ class Pacemaker:
         
     def statusResource(self, resource_name):
         self.resource_name = resource_name
-        
-        try:
-            res_output = check_output(['pcs', 'status', 'resources'], universal_newlines=True)
-            nodes_output = check_output(['pcs', 'status', 'nodes'], universal_newlines=True)
-        
-            res_output.split()[-2] # Started / Stop
-            res_output.split()[-1].strip('()') # host
-            nodes_output.splitlines()[1].split(':')[-1] # online
-            nodes_output.splitlines()[5].split(':')[-1] # offline
-            nodes = (nodes_output.splitlines()[1].split(':')[-1] + nodes_output.splitlines()[5].split(':')[-1]).strip()
-            ret_val = {'status':res_output.split()[-2], 'started':res_output.split()[-1].strip('()'), 'hosts': nodes}
-            ret = createReturn(code=200, val=ret_val)
-            print(json.dumps(json.loads(ret), indent=4))
 
-        except Exception as e:
-            ret = createReturn(code=500, val='ERROR')
-            print ('EXCEPTION : ',e)
+
+        status_output = pcs('status').stdout.decode().splitlines()
+        nodes_output = pcs('status', 'nodes', 'corosync').stdout.decode().splitlines()
+
+        for line in status_output:
+            if self.resource_name in line:
+                current_host = line.split(' ')[-1]
+                status_pcs = line.split(' ')[-2]
+
+        for line in nodes_output:
+            if "Online" in line:
+                online_host = line.split(':')[-1]
+            if "Offline" in line:
+                offline_host = line.split(':')[-1]
+        corosync_hosts = (online_host + offline_host).strip()
+
+        ret_val = {'status':status_pcs, 'started':current_host, 'hosts': corosync_hosts}
+        ret = createReturn(code=200, val=ret_val)
+        print(json.dumps(json.loads(ret), indent=4))
 
         return ret
