5455ab2a04974876fe9bff27af463bde81aed742
ktcheon <ktc7270@gmail.com>
2021-03-15 23:24:33 -0700

    commit on 'PCS'

--- python/pcs/pacemaker.py
+++ python/pcs/pacemaker.py
@@ -6,6 +6,7 @@ from able_return import *
 from sh import pcs
 from sh import systemctl
 import sys
+import xml.etree.ElementTree as ET
 
 def parseArgs():
     parser = argparse.ArgumentParser(description='Pacemaker cluster for Cloud Center VM',
@@ -14,7 +15,6 @@ def parseArgs():
     parser.add_argument('action', choices=['config', 'create', 'enable', 'disable', 'move', 'cleanup', 'status'], help='choose one of the actions')
     parser.add_argument('--cluster', metavar='name', type=str, help='The name of the cluster to be created')
     parser.add_argument('--hosts', metavar='name', type=str, nargs='*', help='Hostnames to form a cluster')
-    #parser.add_argument('hosts', metavar='name', type=str, help='Hostnames to form a cluster', nargs='*')
     parser.add_argument('--resource', metavar='name', type=str, help='The name of the resource to be created')
     parser.add_argument('--xml', metavar='name', type=str, help='Cloud Center VM\'s xml file PATH')
     parser.add_argument('--target', metavar='name', type=str, help='Target hostname to migrate Cloud Center VM')
@@ -123,6 +123,8 @@ class Pacemaker:
     def statusResource(self, resource_name):
         self.resource_name = resource_name
 
+        status_list = {'Starting':'200', 'Started':'200', 'Stopping':'201', 'Stopped':'201', 'FAILED':'500'}
+
         status_output = pcs('status').stdout.decode().splitlines()
         nodes_output = pcs('status', 'nodes', 'corosync').stdout.decode().splitlines()
 
@@ -138,14 +140,14 @@ class Pacemaker:
                 offline_host = line.split(':')[-1]
         corosync_hosts = (online_host + offline_host).strip()
 
-        if current_host not in corosync_hosts:
+        if status_pcs in status_list.keys():
+            ret_val = {'status':status_pcs, 'started':current_host.strip('()'), 'hosts': corosync_hosts}
+            ret = createReturn(code=status_list.get(status_pcs), val=ret_val)
+            print(json.dumps(json.loads(ret), indent=4))
+
+        elif status_pcs not in status_list.keys():   
             ret_val = {'status':'Error', 'started':'Unknown', 'hosts': 'Unknown'}
             ret = createReturn(code=500, val=ret_val)
             print(json.dumps(json.loads(ret), indent=4))
-            sys.exit(1)
-
-        ret_val = {'status':status_pcs, 'started':current_host, 'hosts': corosync_hosts}
-        ret = createReturn(code=200, val=ret_val)
-        print(json.dumps(json.loads(ret), indent=4))
 
         return ret
