b85eb351324711201c2203d341c1af460e5c8ebe
Dajeong-Park <ekwjd7182@gmail.com>
2021-03-19 15:16:05 +0900

    IP 조회 로직 변경
    IP 조회 로직 변경

--- python/url/create-address.py
+++ python/url/create-address.py
@@ -1,107 +1,89 @@
+'''
+Copyright (c) 2021 ABLECLOUD Co. Ltd
+
+이 파일은 스토리지 및 클라우드 센터 관련 연결 주소를 생성하는 기능을 수행합니다.
+최초 작성일 : 2021. 03. 19
+'''
 ##!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
- * File Name : create-address.py
- * Date Created : 2021.03.12
- * Writer  : 박다정
- * Description : 스토리지 및 클라우드 관련 연결 주소를 생성하는 스크립트
-"""
+
+import os
 import json
+import socket
 import logging
 import argparse
-from subprocess import check_output
 from ablestack import *
 
+env = os.environ.copy()
+env['LANG'] = "en_US.utf-8"
+env['LANGUAGE'] = "en"
+
+# 함수명 : createArgumentParser
+# 주요 기능 : 입력된 argument를 파싱하여 dictionary 처럼 사용하게 만들어 주는 parser 생성
 def createArgumentParser():
    
-    """
-    입력된 argument를 파싱하여 dictionary 처럼 사용하게 만들어 주는 parser를 생성하는 함수
-    :return: argparse.ArgumentParser
-    """
-    # 프로그램 설명
     tmp_parser = argparse.ArgumentParser(description='스토리지 및 클라우드 관련 연결 주소를 생성하는 프로그램',
                                          epilog='copyrightⓒ 2021 All rights reserved by ABLECLOUD™')
 
-    # 선택지 추가(동작 선택)
     tmp_parser.add_argument('action', choices=['cloudCenterVm', 'cloudCenter', 'storageCenterVm', 'storageCenter'], help="Create storage and cloud related connection addresses")
-
-    # output 민감도 추가(v갯수에 따라 output및 log가 많아짐)
-    tmp_parser.add_argument("-v", "--verbose", action='count', default=0,
-                            help="increase output verbosity")
-
-    # flag 추가(샘플임, 테스트용으로 json이 아닌 plain text로 출력하는 플래그 역할)
-    tmp_parser.add_argument("-H", "--Human", action='store_const',
-                            dest='H', const=True,
-                            help="Human readable")
-
-    # Version 추가
-    tmp_parser.add_argument("-V", "--Version", action='version',
-                            version="%(prog)s 1.0")
+    tmp_parser.add_argument("-v", "--verbose", action='count', default=0, help="increase output verbosity")
+    tmp_parser.add_argument("-H", "--Human", action='store_const', dest='H', const=True, help="Human readable")
+    tmp_parser.add_argument("-V", "--Version", action='version', version="%(prog)s 1.0")
 
     return tmp_parser
 
 
-def cloudCenterVmUrl(H=False):
-
-    # ccvm cockpit 주소 생성
-    # ccvm cockpit port fix 필요
-    ip = check_output(['grep', '-w', 'ablecloud', '/etc/hosts'], universal_newlines=True).split(' ')
-    value = 'https://'+ip[0]+':9898'
-   
-    if H: 
-        return json.dumps(json.loads(createReturn(code=200, val=value)), indent=4) 
-
-    return createReturn(code=200, val=value)
-
-def cloudCenterUrl(H=False):   
-
-    # ccvm cloudstack 주소 생성
-    # https 구성시 automatic redirect 설정  
-    ip = check_output(['grep', '-w', 'ablecloud', '/etc/hosts'], universal_newlines=True).split(' ')
-    value = 'http://'+ip[0]+':8080'
-
-    if H: 
-        return json.dumps(json.loads(createReturn(code=200, val=value)), indent=4) 
-
-    return createReturn(code=200, val=value)
-       
-def storageCenterVmUrl(H=False):   
-
-    # scvm cockpit 주소 생성
-    # 호스트의 scvm ip 조회
-    ip = check_output(['grep', '-w', 'host', '/etc/hosts'], universal_newlines=True).split(' ')
-    value = 'https://'+ip[0]+':9090'
-
-    if H: 
-        return json.dumps(json.loads(createReturn(code=200, val=value)), indent=4) 
-
-    return createReturn(code=200, val=value)
-
-def storageCenterUrl(H=False):  
-     
-    # scvm ceph 주소 생성
-    # Active MGR 조회 (ceph -s details -f pretty-json data.services.mgr 항목의 node명 확인하여 ip 조회)
-    ip = check_output(['grep', '-w', 'host', '/etc/hosts'], universal_newlines=True).split(' ')
-    value = 'https://'+ip[0]+':8443'
-
-    if H: 
-        return json.dumps(json.loads(createReturn(code=200, val=value)), indent=4) 
-
-    return createReturn(code=200, val=value)
-
-
-def createAddressAction(action, H):
-    if action == 'cloudCenterVm':
-        return cloudCenterVmUrl(H=H)
-
-    elif action == 'cloudCenter':
-        return cloudCenterUrl(H=H)
+# 함수명 : cloudCenter
+# 주요 기능 : 클라우드센터 및 가상머신 연결 주소 생성 
+def cloudCenter(action, H=False):
     
-    elif action == 'storageCenterVm':
-        return storageCenterVmUrl(H=H)
+    ip = socket.gethostbyname('ccvm-mngt')
+
+    if action == 'cloudCenter':
+        # 클라우드센터 가상머신 
+        value = 'http://'+ip+':8080'
+    else:
+        # 클라우드센터 (port fix 필요)
+        value = 'https://'+ip+':9090'
+
+    if H: 
+        return json.dumps(json.loads(createReturn(code=200, val=value, retname=action)), indent=4) 
+
+    return createReturn(code=200, val=value, retname=action)
+
+
+# 함수명 : storageCenter
+# 주요 기능 : 스토리지센터 및 가상머신 연결 주소 생성 
+def storageCenter(action, H=False):
+
+    if action == 'storageCenter':
+        # 스토리지센터 가상머신
+        # ceph -s details -f pretty-json 명령어 테스트 예정 (data.services.mgr 항목의 node명 으로 scvm의 ip 조회)
+        # ceph mgr services 명령어 테스트 예정 
+        node = "scvm1"
+        ip = socket.gethostbyname(node+'-pn')
+        value = 'https://'+ip+':8443'
+    else:
+        # 스토리지센터
+        num = socket.gethostname()[-1:]
+        ip = socket.gethostbyname('scvm'+num+'-pn')
+        value = 'https://'+ip+':9090'
+
+    if H: 
+        return json.dumps(json.loads(createReturn(code=200, val=value, retname=action)), indent=4) 
+
+    return createReturn(code=200, val=value, retname=action)
+
+
+# 함수명 : createAddressAction
+# 주요 기능 : 파라미터에 따른 함수 호출 
+def createAddressAction(action, H):
+
+    if action == 'cloudCenterVm' or action == 'cloudCenter':
+        return cloudCenter(action, H=H)
 
     else:
-        return storageCenterUrl(H=H)
+        return storageCenter(action, H=H)
     
 
 if __name__ == '__main__':
