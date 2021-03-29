4f588fd3cb73f2622440c2e9830faf12f51649a3
jschoiRR <juiralane@gmail.com>
2021-03-29 15:07:09 +0900

    파일별 주석 추가 및 라인 정리반영

--- python/storage_center_cluster_status/scc_status_detail.py
+++ python/storage_center_cluster_status/scc_status_detail.py
@@ -1,10 +1,14 @@
+# Copyright (c) 2021 ABLECLOUD Co. Ltd
+
+# 스토리지 클러스터상태 상세조회를 위한 파일입니다.
+# 최초 작성일 : 2021. 03. 19
+
 import sys
 import argparse
 import json
 import logging
 import os
 import json
-
 import subprocess
 from subprocess import check_output
 from subprocess import call
@@ -25,20 +29,22 @@ def parseArgs():
     
     parser.add_argument('action', choices=['detail'])
     
-    # output 민감도 추가(v갯수에 따라 output및 log가 많아짐)
+    ''' output 민감도 추가(v갯수에 따라 output및 log가 많아짐) '''
     parser.add_argument("-v", "--verbose", action='count', default=0, help="increase output verbosity")
 
-    # flag 추가(샘플임, 테스트용으로 json이 아닌 plain text로 출력하는 플래그 역할)
+    ''' flag 추가(샘플임, 테스트용으로 json이 아닌 plain text로 출력하는 플래그 역할) '''
     parser.add_argument("-H", "--Human", action='store_const', dest='H', const=True, help="Human readable")
 
-    # Version 추가
+    ''' Version 추가 '''
     parser.add_argument("-V", "--Version", action='version', version="%(prog)s 1.0")
     return parser.parse_args()
 
-# 함수명 : statusDeteil
-# 주요 기능 : 스토리지 클러스트 상태 상세조회
-def statusDeteil():     
 
+'''
+함수명 : statusDetail
+주요 기능 : 스토리지 클러스트 상태 상세조회
+'''
+def statusDetail(): 
     ret_val = ''
     cluster_status = ''
     osd = ''
@@ -55,66 +61,55 @@ def statusDeteil():
     output_json = ''
 
     try:
-
-        rc = call(['ceph -s | grep noout'], universal_newlines=True, shell=True, env=env, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)  
-        
-        if rc == 0: # found
+        ''' 클러스터 유지보수모드인지 체크 리턴값이 0 이면 true, 1이면 false값 세팅 '''
+        rc = call(['ceph -s | grep noout'], universal_newlines=True, shell=True, env=env, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)          
+        if rc == 0:
             maintenance_status = True
-        elif rc == 1: # not found
+        elif rc == 1:
             maintenance_status = False
 
-
-        rc = call(['ceph -s -f json-pretty'], universal_newlines=True, shell=True, env=env, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-        
-        if rc == 0: # found
+        ''' 클러스터 상태 체크 리턴값이 0 이면 정상, 아니면 클러스터 세팅이 안된것으로 확인(no signal) '''
+        rc = call(['ceph -s -f json-pretty'], universal_newlines=True, shell=True, env=env, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)        
+        if rc == 0:
+            ''' ceph -s시 출력값을 json으로 리턴 '''
             output = check_output(['ceph -s -f json-pretty'], universal_newlines=True, shell=True, env=env)
             output_json = json.loads(output)
             cluster_status= output_json['health']['status']
-        else : # not found
+        else :
             cluster_status= "no signal"
 
         mon_gw1= output_json['monmap']['num_mons']
         mon_gw2= output_json['quorum_names']
-
-     
-
-
         osd= output_json['osdmap']['num_osds']
-        osd_up= output_json['osdmap']['num_up_osds']   
+        osd_up= output_json['osdmap']['num_up_osds']
+        pools= output_json['pgmap']['num_pools']
 
-        pools= output_json['pgmap']['num_pools']        
-
-
-        rc = call(['ceph mgr stat'], universal_newlines=True, shell=True, env=env, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-        
-        if rc == 0: # found
+        ''' 클러스터 MGR Daemon값 체크 0 이면 정상, 아니면 undefine '''
+        rc = call(['ceph mgr stat'], universal_newlines=True, shell=True, env=env, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)        
+        if rc == 0:
+            # MGR deamon 상태값 조회
             output_mgr = check_output(['ceph mgr stat'], universal_newlines=True, shell=True, env=env)        
             output_mgr = json.loads(output_mgr)
             mgr = output_mgr['active_name'];
             mgr_cnt= int(output_json['mgrmap']['num_standbys']) + 1
-        else : # not found
+        else : 
             mgr= "undefine"
             mgr_cnt = "undefine"
 
-
-
-
-        rc = call(['ceph df | grep TOTAL'], universal_newlines=True, shell=True, env=env, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-        
-        if rc == 0: # found
+        '''클러스터 디스크용량 체크 0 이면 정상, 아니면 undefine '''
+        rc = call(['ceph df | grep TOTAL'], universal_newlines=True, shell=True, env=env, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)        
+        if rc == 0:
+            '''클러스터 디스크 사용가능 용량, 사용량, 사용량 % 확인'''
             output = check_output(['ceph df | grep TOTAL'], universal_newlines=True, shell=True, env=env)        
             output = ' '.join(output.split()).split()        
             available = output[3] + " "+ output[4]
             used = output[7] + " "+ output[8]
             usage_percentage = output[9]
-        else : # not found
+        else :
             available= "undefine"
             used = "undefine"
             usage_percentage = "undefine"
-
         
-        
-        # 실제 데이터 세팅
         ret_val = {
             'cluster_status': cluster_status, 
             'osd': osd, 
@@ -130,7 +125,6 @@ def statusDeteil():
             'maintenance_status': maintenance_status,
             'json_raw' : output_json}
         ret = createReturn(code=200, val=ret_val, retname='Storage Cluster Status Detail')
-
     except Exception as e:
         ret_val = {
             'cluster_status': cluster_status, 
@@ -146,20 +140,14 @@ def statusDeteil():
             'usage_percentage': usage_percentage, 
             'maintenance_status': maintenance_status,
             'json_raw' : output_json}
-        ret = createReturn(code=500, val=ret_val, retname='Storage Cluster Status Detail')
-        #print ('EXCEPTION : ',e)
-    
+        ret = createReturn(code=500, val=ret_val, retname='Storage Cluster Status Detail')        
     return print(json.dumps(json.loads(ret), indent=4))
 
-
 if __name__ == '__main__':
-
-    # parser 생성
-    args = parseArgs()
-    ##print(args);
+    '''parser 생성'''
+    args = parseArgs()    
     if args.action == 'detail':        
-        ret = statusDeteil()
-    
-    #print(ret)
+        ret = statusDetail()    
+    '''print(ret)'''
     
 
