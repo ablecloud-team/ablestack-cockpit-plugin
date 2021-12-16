88480e57f135ed90a9966c4ccdfcbfcc7d88f24e
stardom3645 <stardom3645@gmail.com>
2021-12-16 10:35:38 +0900

    textarea 텍스트 문자열에 공백제거 추가

--- src/common/wizard-common.js
+++ src/common/wizard-common.js
@@ -507,7 +507,7 @@ function setSshKeyFileReader(input, file_name, callBackFunction) {
                 }
             }
         });
-        $('#div-textarea-cluster-config-confirm-hosts-file'+option+'').val(arr);
+        $('#div-textarea-cluster-config-confirm-hosts-file'+option+'').val(arr.trim());
     } else if (radio_value == "existing") {
         // hosts file 준비 방법 표시 및 값 설정
         $('#span-hosts-file'+option+'').text("기존 파일 사용");
@@ -527,6 +527,6 @@ function setSshKeyFileReader(input, file_name, callBackFunction) {
                 }
             }
         });
-        $('#div-textarea-cluster-config-confirm-hosts-file'+option+'').val(arr);
+        $('#div-textarea-cluster-config-confirm-hosts-file'+option+'').val(arr.trim());
     }
 }
